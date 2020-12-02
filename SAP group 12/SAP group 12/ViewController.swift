8d2326d0d3cc5fe6411e6f6ada4cb5a44d19b40c
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2020-12-02 16:44:42 +0800

    Initial commit

---
+++ SAP group 12/SAP group 12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SAP group 12
+//
+//  Created by Shanjiith Pranov on 2/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

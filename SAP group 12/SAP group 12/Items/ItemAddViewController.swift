6370243a8f40b971a39fa2e0570faa2630a3252f
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2020-12-15 23:02:45 +0800

    Cleaned code

--- SAP group 12/SAP group 12/Items/ItemAddViewController.swift
+++ SAP group 12/SAP group 12/Items/ItemAddViewController.swift
@@ -18,55 +18,14 @@ class ItemAddViewController: UIViewController{
     var currentDate = Date()
 
     
-//    var formatter: DateFormatter {
-//        let formatter = DateFormatter()
-//        formatter.timeStyle = .none
-//        formatter.dateStyle = .medium
-//        return formatter
-//    }
-//    var simpleCurrentDate = ""
-//    var simpleCurrentDateSplit: [Substring] = []
-//    var simpleCurrentDateMonth = 0
-//    var simpleCurrentDateDays: [Substring] = []
-//    var simpleCurrentDateDay = 0
-//    var simpleCurrentDateYear = 0
-//
-//    var simpleExpiryDate = ""
-//    var simpleExpiryDateSplit: [Substring] = []
-//    var simpleExpiryDateMonth = 0
-//    var simpleExpiryDateDays: [Substring] = []
-//    var simpleExpiryDateDay = 0
-//    var simpleExpiryDateYear = 0
-//
-//    var expiresIn = 0
-//
-//    var months = [".","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+
 
 
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//
-//        simpleCurrentDate = formatter.string(from: currentDate)
-//        simpleCurrentDateSplit = simpleCurrentDate.split(separator: " ")
-//        simpleCurrentDateMonth = months.firstIndex(of: String(simpleCurrentDateSplit[0])) ?? 0
-//        simpleCurrentDateDays = simpleCurrentDateSplit[1].split(separator: ",")
-//        simpleCurrentDateDay = Int(simpleCurrentDateDays[0]) ?? 1
-//        simpleCurrentDateYear = Int(simpleCurrentDateSplit[2]) ?? 2021
-        
-        
-        
 
-        
-        
-
-        
-        
-        
-        
-        // Do any additional setup after loading the view.
         
         
     }
@@ -96,14 +55,7 @@ class ItemAddViewController: UIViewController{
         let diffs = Calendar.current.dateComponents([.day], from: currentDate, to: expiryDatePicker.date)
         
         
-//        simpleExpiryDate = formatter.string(from: expiryDatePicker.date)
-//        simpleExpiryDateSplit = simpleExpiryDate.split(separator: " ")
-//        simpleExpiryDateMonth = months.firstIndex(of: String(simpleExpiryDateSplit[0])) ?? 0
-//        simpleExpiryDateDays = simpleExpiryDateSplit[1].split(separator: ",")
-//        simpleExpiryDateDay = Int(simpleExpiryDateDays[0]) ?? 1
-//        simpleExpiryDateYear = Int(simpleExpiryDateSplit[2]) ?? 2021
-//
-//        expiresIn = (simpleExpiryDateYear - simpleCurrentDateYear)*365 + (simpleExpiryDateMonth - simpleCurrentDateMonth)*30 + (simpleExpiryDateDay - simpleCurrentDateDay)
+
 
         
         
@@ -115,17 +67,6 @@ class ItemAddViewController: UIViewController{
         
         return true
     }
-    
-    
-    
-    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    //        if segue.identifier == "saveunwind" {
-    //            item = Item(name: nameTextField.text ?? "",
-    //                        quantity: quantityTextField.text ?? "",
-    //                        isFavourite: false,
-    //                        expiryDate: expiryDatePicker.date)
-    //        }
-    //    }
     
     
     

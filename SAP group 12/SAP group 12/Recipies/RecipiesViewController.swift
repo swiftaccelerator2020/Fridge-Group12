39beb3acdb7395b1e0f8ede4b80ab86143bd3d6d
sawzedong <saw.zedong@dhs.sg>
2020-12-22 12:08:12 +0800

    Patched recipe loading bug when there are no items

--- SAP group 12/SAP group 12/Recipies/RecipiesViewController.swift
+++ SAP group 12/SAP group 12/Recipies/RecipiesViewController.swift
@@ -23,88 +23,26 @@ class RecipiesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.loadingIndicator.startAnimating()
-        self.loadingIndicator.isHidden = false
-        
-        self.searchFoodItems = []
-        if let loadedItems = Item.loadFromFile() {
-            for item in loadedItems {
-                self.searchFoodItems.append(item.name)
-            }
-        }
-        
-        self.listOfRecipes = []
-        self.recipiesTableView.reloadData()
-        // run get recipes function
-        getRecipes (foodItems: self.searchFoodItems) { (recipeResults) in
-            if var recipeResults = recipeResults {
-                // store array of all items
-                var itemsLeftToCheck = recipeResults.results.count
-                if recipeResults.results.count == 0 {
-                    self.listOfRecipes = [RecipeItem(title: "No recipes could be found with your ingredients. Please check again.", recipeLink: "", ingredients: [""], thumbnailLink: "", canShowDetails: false)]
-                    DispatchQueue.main.async {
-                        self.loadingIndicator.stopAnimating()
-                        self.loadingIndicator.isHidden = true
-                        self.recipiesTableView.reloadData()
-                        self.defaults.setValue(false, forKey: "shouldReloadRecipes")
-                    }
-                } else {
-                    // using a for loop, check all links from item results and only append items with valid recipe links
-                    for i in 0...(recipeResults.results.count - 1) {
-                        let urlToCheck = "\(recipeResults.results[i].recipeLink)"
-                        checkWebsiteValid(url: urlToCheck) { (websiteIsValid) in
-                            itemsLeftToCheck -= 1
-                            if websiteIsValid! {
-                                print(recipeResults.results[i])
-                                var titleOfItem = recipeResults.results[i].title
-                                // regex function 1 - remove all whitespace characters that are not spacebar characters
-                                let regex1 = try! NSRegularExpression(pattern: #"[\r\n\t\f\v]"#, options: [.caseInsensitive])
-                                let mString1 = NSMutableString(string: titleOfItem)
-                                regex1.replaceMatches(in: mString1, options: [], range: NSMakeRange(0, mString1.length), withTemplate: "")
-                                titleOfItem = String(mString1)
-                                titleOfItem = titleOfItem.filter {CharacterSet.decimalDigits.inverted.contains($0.unicodeScalars.first!)}
-                                titleOfItem = titleOfItem.replacingOccurrences(of: "  ", with: " ")
-                                
-                                // regex function 2 - remove all whitespace characters at the start
-                                let regex2 = try! NSRegularExpression(pattern: #"^\s*"#, options: [.caseInsensitive])
-                                let mString2 = NSMutableString(string: titleOfItem)
-                                regex2.replaceMatches(in: mString2, options: [], range: NSMakeRange(0, mString2.length), withTemplate: "")
-                                titleOfItem = String(mString2)
-                                titleOfItem = titleOfItem.filter {CharacterSet.decimalDigits.inverted.contains($0.unicodeScalars.first!)}
-                                titleOfItem = titleOfItem.replacingOccurrences(of: "  ", with: " ")
-                                
-                                print(titleOfItem)
-                                recipeResults.results[i].title = titleOfItem
-                                self.listOfRecipes.append(recipeResults.results[i])
-                            }
-                            if(itemsLeftToCheck == 0) {
-                                // TODO: implement reloading table view here - zedong
-                                DispatchQueue.main.async {
-                                    self.loadingIndicator.stopAnimating()
-                                    self.loadingIndicator.isHidden = true
-                                    self.recipiesTableView.reloadData()
-                                    self.defaults.setValue(false, forKey: "shouldReloadRecipes")
-                                }
-                                for item in self.listOfRecipes {
-                                    print(item)
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        callRecipes()
     }
     
     override func viewWillAppear (_ animated: Bool) {
         
         // Do any additional setup after loading the view
         
-        let shouldReloadRecipes = defaults.bool(forKey: "shouldReloadRecipes")
+        
         
         recipiesTableView.delegate = self
         recipiesTableView.dataSource = self
         recipiesTableView.separatorStyle = .none
+        
+        callRecipes()
+        
+        
+    }
+    
+    func callRecipes() {
+        let shouldReloadRecipes = defaults.bool(forKey: "shouldReloadRecipes")
         
         self.searchFoodItems = []
         if let loadedItems = Item.loadFromFile() {
@@ -189,8 +127,6 @@ class RecipiesViewController: UIViewController {
             }
             
         }
-        
-        
     }
     
     
